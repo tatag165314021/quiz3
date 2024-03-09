7f01b5c341b14e90dc06813909f4b9f8243bd510
unknown <tataghardoyo@gmail.com>
2024-03-10 05:17:08 +0700

    deploy railway

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
 		fmt.Println("success read file environtment")
 	}
 
-	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", os.Getenv("PGHOST"), os.Getenv("PGPORT"), os.Getenv("PGUSER"), os.Getenv("PGPASSWORD"), os.Getenv("PGDATABASE"))
 
 	DB, err = sql.Open("postgres", psqlInfo)
 	err = DB.Ping()
@@ -42,6 +42,6 @@ func main() {
 
 	defer DB.Close()
 
-	var PORT = ":8080"
-	routers.StartServer().Run(PORT)
+	// var PORT = ":8080"
+	routers.StartServer().Run("PORT")
 }
